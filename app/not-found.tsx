dbe7d194c46853c69aedefb8022b5cecaed03c9f
unknown <mohammedhamed6726@gmail.com>
2026-06-24 20:28:13 +0300

    improve error and not found pages

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,6 +9,16 @@ export default function NotFound() {
   return (
     <section className="relative flex min-h-svh flex-col items-center justify-center bg-background  p-6 md:p-10 overflow-hidden">
       <MaxWContainer className="flex flex-col items-center justify-center gap-4 *:text-center relative px-4 sm:px-6 lg:px-8">
+        <div className=" gap-2 flex justify-center items-center ">
+          <Image
+            src={imgsrc}
+            alt="Notevo Logo"
+            priority
+            width={20}
+            height={20}
+          />
+          <p className="text-sm text-foreground">Notevo.me</p>
+        </div>
         <Card className="overflow-hidden bg-card border-border">
           <CardContent className="relative pt-4 pb-16">
             <div className="flex flex-col items-center justify-center gap-4 text-center">
@@ -25,15 +35,16 @@ export default function NotFound() {
           </CardContent>
         </Card>
         <div className="w-full flex justify-center items-center">
-          <Image
-            src={imgsrc}
-            alt="Notevo Logo"
-            priority
-            width={16}
-            height={16}
-          />
-          <p className="text-muted-foreground text-xs font-medium px-2">
-            {` ! Hi this is Notevo team we're really sorry for this | go back to the home page `}
+          <p className="text-muted-foreground text-xs font-medium px-2 max-w-lg">
+            ! Hi this is Notevo's team we're really sorry but i think we dont
+            have a this page If the problem keeps happening, email us at{" "}
+            <Link
+              href="mailto:support@notevo.me"
+              className=" font-bold text-foreground hover:underline"
+            >
+              support@notevo.me
+            </Link>{" "}
+            and we'll help you out. <br /> but for now Return Home
           </p>
         </div>
       </MaxWContainer>
