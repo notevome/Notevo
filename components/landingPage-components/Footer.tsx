ccee741d7e8168835427fe15b101d3af2a32743e
unknown <mohammedhamed6726@gmail.com>
2026-07-26 21:07:56 +0300

    refresh landing page footer

--- components/landingPage-components/Footer.tsx
+++ components/landingPage-components/Footer.tsx
@@ -34,8 +34,7 @@ export default function Footer() {
           </defs>
         </svg>
       </div>
-      <footer className="relative w-full z-10 text-foreground mt-20 md:mt-28 pb-10 bg-[#EFEFEF]">
-        <SignUpToday />
+      <footer className="relative w-full z-10 text-foreground mt-20 md:mt-28 pb-48 bg-[#EFEFEF]">
         <div className="max-w-6xl mx-auto py-5 px-6 md:px-12 lg:px-16">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
             <div>
@@ -163,6 +162,9 @@ export default function Footer() {
           </div>
         </div>
       </footer>
+      <h1 className="z-50 bg-gradient-to-b from-primary/30 from-60% to-100% to-transparent text-transparent bg-clip-text inline-block text-nowrap  text-[200px] absolute -bottom-6 right-0 align-bottom text-center pointer-events-none select-none w-full overflow-hidden font-bold tracking-tighter leading-[9rem] -indent-8">
+        Notes Without The Hassle
+      </h1>
     </div>
   );
 }
