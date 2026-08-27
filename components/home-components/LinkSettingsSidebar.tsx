edd7207aaf0e0db89ad6eb6a41c548dd0246c63b
unknown <mohammedhamed6726@gmail.com>
2026-08-28 00:03:25 +0300

    clean up link sidebar button

--- components/home-components/LinkSettingsSidebar.tsx
+++ components/home-components/LinkSettingsSidebar.tsx
@@ -83,7 +83,7 @@ export default function LinkSettingsSidebar({
             <Button
               onClick={handleFavoritePin}
               variant="SidebarMenuButton"
-              className="px-1.5 h-7 hover:bg-card !rounded-none"
+              className="px-1.5 h-7 hover:bg-card"
               aria-label="pin-link"
               {...pinTooltip.triggerProps}
             >
